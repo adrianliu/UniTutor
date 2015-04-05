f92517ad621bf62ff3329a3f20189e98d55b07e0
adrianliu <adrian.lxp729@gmail.com>
2015-04-05 16:45:59 +1200

    first commitment

---
+++ UniTutor/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  UniTutor
+//
+//  Created by xiaopeng liu on 4/04/15.
+//  Copyright (c) 2015 xiaopeng liu. All rights reserved.
+//
+
+#ifndef UniTutor_Bridging_Header_h
+#define UniTutor_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+#endif
